90bcc67839a0b3af469b55f60024972743f4f3b1
cor18018 <cor18018@byui.edu>
2021-12-09 15:02:07 -0700

    Styling

--- minmax.js
+++ minmax.js
@@ -1,70 +1,65 @@
-// Tic Tac Toe AI with Minimax Algorithm
-// The Coding Train / Daniel Shiffman
-// https://thecodingtrain.com/CodingChallenges/154-tic-tac-toe-minimax.html
-// https://youtu.be/I64-UTORVfU
-// https://editor.p5js.org/codingtrain/sketches/0zyUhZdJD
+//Minimax Algorithm
 
+let score = {
+  X: 100,
+  O: -100,
+  tie: 0
+};
+
+//Allows AI to chose the best move for its next move
 function bestMove() {
-    // AI to make its turn
-    let bestScore = -Infinity;
+    let bestChoice = -Infinity;
     let move;
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
-        // Is the spot available?
         if (board[i][j] == '') {
           board[i][j] = ai;
           let score = minimax(board, 0, false);
           board[i][j] = '';
-          if (score > bestScore) {
-            bestScore = score;
+          if (score > bestChoice) {
+            bestChoice = score;
             move = { i, j };
           }
         }
       }
     }
     board[move.i][move.j] = ai;
-    currentPlayer = human;
+    player = human;
   }
   
-  let scores = {
-    X: 10,
-    O: -10,
-    tie: 0
-  };
   
-  function minimax(board, depth, isMaximizing) {
-    let result = checkWinner();
+  function minimax(board, depth, maximize) {
+    let result = winner();
     if (result !== null) {
-      return scores[result];
+      return score[result];
     }
   
-    if (isMaximizing) {
-      let bestScore = -Infinity;
+    if (maximize) {
+      let bestChoice = -Infinity;
       for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
-          // Is the spot available?
           if (board[i][j] == '') {
             board[i][j] = ai;
             let score = minimax(board, depth + 1, false);
             board[i][j] = '';
-            bestScore = max(score, bestScore);
+            bestChoice = max(score, bestChoice);
           }
         }
       }
-      return bestScore;
+      return bestChoice;
     } else {
-      let bestScore = Infinity;
+      let bestChoice = Infinity;
       for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
-          // Is the spot available?
           if (board[i][j] == '') {
             board[i][j] = human;
             let score = minimax(board, depth + 1, true);
             board[i][j] = '';
-            bestScore = min(score, bestScore);
+            bestChoice = min(score, bestChoice);
           }
         }
       }
-      return bestScore;
+      return bestChoice;
     }
   }
+
